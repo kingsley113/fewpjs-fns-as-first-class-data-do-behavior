f65fe2043e202378da0460ebc5612e67fc4b3cb2
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-08-28 21:54:49 -0700

    commit tree da78a993d251e03da8b68f311b4770b61c11573f

--- index.js
+++ index.js
@@ -3,11 +3,27 @@
 document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+	const timeString = document.getElementById('time').value
+	displayMessage(greet(timeString))
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString) {
+	const time = parseInt(timeString.split(':')[0]);
+
+	if (time < 12) {
+		return "Good Morning";
+	} else if (time >= 12 && time <= 17) {
+		return "Good Afternoon";
+	} else {
+		return "Good Evening"
+	}
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+	const greeting = document.querySelector("h1#greeting");
+	greeting.innerHTML = string;
+}
